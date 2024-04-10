4cf46c3cbac08381e49bc77e91db2d06b924ff37
gram <git@orsinium.dev>
2024-04-10 09:36:55 +0200

    improve validation error message

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(not(test), no_std)]
+#![feature(ascii_char)]
 mod meta;
 mod validators;
 
 pub use meta::Meta;
-pub use validators::{valid_id, valid_name, valid_path_part};
+pub use validators::*;
--- src/validators.rs
+++ src/validators.rs
@@ -1,3 +1,37 @@
+use core::fmt::Display;
+
+pub enum ValidationError {
+    TrailingMinus,
+    DoubleMinus,
+    Empty,
+    InvalidChar(u8),
+    InvalidFirstChar(u8),
+    TooLong,
+    TrailingSpace,
+    TrailingDot,
+}
+
+impl Display for ValidationError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            ValidationError::TrailingMinus => write!(f, "must not start or end with minus"),
+            ValidationError::DoubleMinus => write!(f, "must not contain '--'"),
+            ValidationError::Empty => write!(f, "must not be empty"),
+            ValidationError::InvalidChar(c) => match char::from_u32(*c as u32) {
+                Some(c) => write!(f, "must not contain {c}"),
+                None => write!(f, "must contain only valid ASCII characters"),
+            },
+            ValidationError::InvalidFirstChar(c) => match char::from_u32(*c as u32) {
+                Some(c) => write!(f, "must not start with {c}"),
+                None => write!(f, "must start with an ASCII character"),
+            },
+            ValidationError::TooLong => write!(f, "too long"),
+            ValidationError::TrailingSpace => write!(f, "must not start or end with space"),
+            ValidationError::TrailingDot => write!(f, "must not start or end with dot"),
+        }
+    }
+}
+
 /// Validate the author or the app ID.
 ///
 /// The ID should have at least one character and may contain only
@@ -5,22 +39,22 @@
 ///
 /// Without ID validation, an app may use a malformed ID (like "../../../")
 /// to gain access to arbitrary files of other apps, including secrets.
-pub fn valid_id(s: &str) -> bool {
+pub fn validate_id(s: &str) -> Result<(), ValidationError> {
     if s.starts_with('-') || s.ends_with('-') {
-        return false;
+        return Err(ValidationError::TrailingMinus);
     }
     if s.contains("--") {
-        return false;
+        return Err(ValidationError::DoubleMinus);
     }
     if s.is_empty() {
-        return false;
+        return Err(ValidationError::Empty);
     }
     for c in s.bytes() {
         if !c.is_ascii_lowercase() && !c.is_ascii_digit() && c != b'-' {
-            return false;
+            return Err(ValidationError::InvalidChar(c));
         }
     }
-    true
+    Ok(())
 }
 
 /// Validate a name (app name, author name).
@@ -29,52 +63,52 @@ pub fn valid_id(s: &str) -> bool {
 /// Keep in mind that the validation DOES NOT ensure
 /// that the text is a safe file name.
 /// You need to sanitize it first to use in a file path.
-pub fn valid_name(s: &str) -> bool {
+pub fn validate_name(s: &str) -> Result<(), ValidationError> {
     if s.len() > 40 {
-        return false;
+        return Err(ValidationError::TooLong);
     }
     if s.ends_with(' ') {
-        return false;
+        return Err(ValidationError::TrailingSpace);
     }
     let mut b = s.bytes();
     match b.next() {
         // Must start with a letter.
         Some(c) => {
             if !c.is_ascii_alphabetic() {
-                return false;
+                return Err(ValidationError::InvalidFirstChar(c));
             }
         }
         // Must not be empty.
-        None => return false,
+        None => return Err(ValidationError::Empty),
     }
     for c in b {
         if c.is_ascii_alphanumeric() {
             continue;
         }
         if !c.is_ascii_punctuation() && c != b' ' {
-            return false;
+            return Err(ValidationError::InvalidChar(c));
         }
     }
-    true
+    Ok(())
 }
 
 /// Validate a path component (file or directory name).
-pub fn valid_path_part(s: &str) -> bool {
+pub fn validate_path_part(s: &str) -> Result<(), ValidationError> {
     if s.starts_with('.') {
-        return false;
+        return Err(ValidationError::TrailingDot);
     }
     if s.is_empty() {
-        return false;
+        return Err(ValidationError::Empty);
     }
     for c in s.bytes() {
         if c.is_ascii_alphanumeric() {
             continue;
         }
         if c != b'.' && c != b'_' && c != b'-' {
-            return false;
+            return Err(ValidationError::InvalidChar(c));
         }
     }
-    true
+    Ok(())
 }
 
 #[cfg(test)]
@@ -83,86 +117,86 @@ mod tests {
 
     #[test]
     fn test_valid_id() {
-        assert!(valid_id("app"));
-        assert!(valid_id("some-app"));
-        assert!(valid_id("some-app-13"));
-        assert!(valid_id("13app"));
-        assert!(valid_id("relatively-long-app-name"));
-        assert!(valid_id("a"));
+        assert!(validate_id("app").is_ok());
+        assert!(validate_id("some-app").is_ok());
+        assert!(validate_id("some-app-13").is_ok());
+        assert!(validate_id("13app").is_ok());
+        assert!(validate_id("relatively-long-app-name").is_ok());
+        assert!(validate_id("a").is_ok());
     }
 
     #[test]
     fn test_invalid_id() {
-        assert!(!valid_id("app.name"));
-        assert!(!valid_id("app--name"));
-        assert!(!valid_id("-appname"));
-        assert!(!valid_id("-app-name"));
-        assert!(!valid_id("-appname"));
-        assert!(!valid_id("app-name-"));
-        assert!(!valid_id("appname-"));
-        assert!(!valid_id("-appname-"));
-        assert!(!valid_id("app name"));
-        assert!(!valid_id("appname "));
-        assert!(!valid_id(" appname"));
-        assert!(!valid_id("App"));
-        assert!(!valid_id("AppName"));
-        assert!(!valid_id("APPNAME"));
-        assert!(!valid_id(""));
-        assert!(!valid_id(" "));
-        assert!(!valid_id("-"));
-        assert!(!valid_id("--"));
-        assert!(!valid_id("?hello"));
+        assert!(validate_id("app.name").is_err());
+        assert!(validate_id("app--name").is_err());
+        assert!(validate_id("-appname").is_err());
+        assert!(validate_id("-app-name").is_err());
+        assert!(validate_id("-appname").is_err());
+        assert!(validate_id("app-name-").is_err());
+        assert!(validate_id("appname-").is_err());
+        assert!(validate_id("-appname-").is_err());
+        assert!(validate_id("app name").is_err());
+        assert!(validate_id("appname ").is_err());
+        assert!(validate_id(" appname").is_err());
+        assert!(validate_id("App").is_err());
+        assert!(validate_id("AppName").is_err());
+        assert!(validate_id("APPNAME").is_err());
+        assert!(validate_id("").is_err());
+        assert!(validate_id(" ").is_err());
+        assert!(validate_id("-").is_err());
+        assert!(validate_id("--").is_err());
+        assert!(validate_id("?hello").is_err());
     }
 
     #[test]
     fn test_valid_name() {
-        assert!(valid_name("app"));
-        assert!(valid_name("a"));
-        assert!(valid_name("some-app"));
-        assert!(valid_name("App"));
-        assert!(valid_name("Some app"));
-        assert!(valid_name("Some App"));
-        assert!(valid_name("SOME APP"));
-        assert!(valid_name("Hello"));
-        assert!(valid_name("Hello?"));
-        assert!(valid_name("Yes? Yes!"));
+        assert!(validate_name("app").is_ok());
+        assert!(validate_name("a").is_ok());
+        assert!(validate_name("some-app").is_ok());
+        assert!(validate_name("App").is_ok());
+        assert!(validate_name("Some app").is_ok());
+        assert!(validate_name("Some App").is_ok());
+        assert!(validate_name("SOME APP").is_ok());
+        assert!(validate_name("Hello").is_ok());
+        assert!(validate_name("Hello?").is_ok());
+        assert!(validate_name("Yes? Yes!").is_ok());
     }
 
     #[test]
     fn test_invalid_name() {
-        assert!(!valid_name(" "));
-        assert!(!valid_name("  "));
-        assert!(!valid_name(""));
-        assert!(!valid_name(" abc"));
-        assert!(!valid_name("abc "));
-        assert!(!valid_name("ab\tcd"));
-        assert!(!valid_name("тест"));
-        assert!(!valid_name("?hello"));
+        assert!(validate_name(" ").is_err());
+        assert!(validate_name("  ").is_err());
+        assert!(validate_name("").is_err());
+        assert!(validate_name(" abc").is_err());
+        assert!(validate_name("abc ").is_err());
+        assert!(validate_name("ab\tcd").is_err());
+        assert!(validate_name("тест").is_err());
+        assert!(validate_name("?hello").is_err());
     }
 
     #[test]
     fn test_valid_path_part() {
-        assert!(valid_path_part("app"));
-        assert!(valid_path_part("a"));
-        assert!(valid_path_part("some-app"));
-        assert!(valid_path_part("App"));
-        assert!(valid_path_part("file.wasm"));
-        assert!(valid_path_part("file_name.wasm"));
-        assert!(valid_path_part("FileName.wasm"));
+        assert!(validate_path_part("app").is_ok());
+        assert!(validate_path_part("a").is_ok());
+        assert!(validate_path_part("some-app").is_ok());
+        assert!(validate_path_part("App").is_ok());
+        assert!(validate_path_part("file.wasm").is_ok());
+        assert!(validate_path_part("file_name.wasm").is_ok());
+        assert!(validate_path_part("FileName.wasm").is_ok());
     }
 
     #[test]
     fn test_invalid_path_part() {
-        assert!(!valid_path_part(".gitignore"));
-        assert!(!valid_path_part(".."));
-        assert!(!valid_path_part("/"));
-        assert!(!valid_path_part("./"));
-        assert!(!valid_path_part("???"));
-        assert!(!valid_path_part("file/../root"));
-        assert!(!valid_path_part("file name"));
-        assert!(!valid_path_part(" file"));
-        assert!(!valid_path_part("file "));
-        assert!(!valid_path_part(""));
-        assert!(!valid_path_part(" "));
+        assert!(validate_path_part(".gitignore").is_err());
+        assert!(validate_path_part("..").is_err());
+        assert!(validate_path_part("/").is_err());
+        assert!(validate_path_part("./").is_err());
+        assert!(validate_path_part("???").is_err());
+        assert!(validate_path_part("file/../root").is_err());
+        assert!(validate_path_part("file name").is_err());
+        assert!(validate_path_part(" file").is_err());
+        assert!(validate_path_part("file ").is_err());
+        assert!(validate_path_part("").is_err());
+        assert!(validate_path_part(" ").is_err());
     }
 }
