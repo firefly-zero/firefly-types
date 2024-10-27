57a2e8cd3f61571339c3a74e235be0f925bbe75a
gram <git@orsinium.dev>
2024-10-27 11:43:35 +0100

    add Board.validate

--- src/boards.rs
+++ src/boards.rs
@@ -1,6 +1,30 @@
 use alloc::borrow::Cow;
 use alloc::boxed::Box;
+use core::fmt::Display;
 use serde::{Deserialize, Serialize};
+
+pub enum BoardValidationError {
+    EmptyName,
+    NameTooLong,
+    MinGtMax,
+}
+
+impl BoardValidationError {
+    #[must_use]
+    pub const fn as_str(&self) -> &'static str {
+        match self {
+            Self::EmptyName => "name must not be empty",
+            Self::NameTooLong => "name is too long",
+            Self::MinGtMax => "min must be less than or equal to max",
+        }
+    }
+}
+
+impl Display for BoardValidationError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "{}", self.as_str())
+    }
+}
 
 #[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
 pub struct Scores {
@@ -100,4 +124,24 @@ pub struct Board<'a> {
 
     /// Human-readable board name.
     pub name: &'a str,
+}
+
+impl<'a> Board<'a> {
+    /// Validate board attributes.
+    ///
+    /// # Errors
+    ///
+    /// Returns [`BoardValidationError`] if any of the attributes are not valid.
+    pub const fn validate(&self) -> Result<(), BoardValidationError> {
+        if self.name.is_empty() {
+            return Err(BoardValidationError::EmptyName);
+        }
+        if self.name.len() > 64 {
+            return Err(BoardValidationError::NameTooLong);
+        }
+        if self.min > self.max {
+            return Err(BoardValidationError::MinGtMax);
+        }
+        Ok(())
+    }
 }
