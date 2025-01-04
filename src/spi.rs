1c332becc320b79c6b38bd72a9d0d6c00d6e0db2
gram <git@orsinium.dev>
2025-01-04 18:17:04 +0100

    document requests

--- src/spi.rs
+++ src/spi.rs
@@ -1,19 +1,28 @@
 use crate::encode::Encode;
 use serde::{Deserialize, Serialize};
 
+/// Request that the main chip sends to the IO chip.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub enum Request<'a> {
+    /// Start listening for messages.
     NetStart,
+    /// Stop accepting new messages and connections.
     NetStop,
+    /// Get MAC address of this device's IO chip.
     NetLocalAddr,
+    /// Broadcast advertisement message.
     NetAdvertise,
+    /// Read an incoming message (if any) from the IO chip.
     NetRecv,
-    NetSend(&'a [u8]),
+    /// Send an outgoing message to the IO chip.
+    NetSend([u8; 6], &'a [u8]),
+    /// Get the latest touchpad and buttons inputs.
     ReadInput,
 }
 
 impl<'a> Encode<'a> for Request<'a> {}
 
+/// Response that the IO chip sends back to the main chip.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub enum Response<'a> {
     NetError(u32),
@@ -21,7 +30,8 @@ pub enum Response<'a> {
     NetStopped,
     NetLocalAddr([u8; 6]),
     NetAdvertised,
-    NetIncoming(&'a [u8]),
+    NetIncoming([u8; 6], &'a [u8]),
+    NetNoIncoming,
     NetSent,
     Input(Option<(i16, i16)>, u8),
 }
