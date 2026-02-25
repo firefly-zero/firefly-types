24463a03a9382c10398cd05d37eb56ab3f73a3a8
gram <git@orsinium.dev>
2026-02-25 10:39:55 +0100

    some wifi and tcp SPI messages

--- src/spi.rs
+++ src/spi.rs
@@ -2,7 +2,7 @@ use crate::encode::Encode;
 use serde::{Deserialize, Serialize};
 
 /// Request that the main chip sends to the IO chip.
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub enum Request<'a> {
     /// Start listening for messages.
     NetStart,
@@ -20,6 +20,19 @@ pub enum Request<'a> {
     NetSendStatus([u8; 6]),
     /// Get the latest touchpad and buttons inputs.
     ReadInput,
+
+    /// Scan the air for available non-hidden Wi-Fi access points.
+    WifiScan,
+    /// Connect to an access point using the given SSID and password.
+    WifiConnect(&'a str, &'a str),
+    /// Disconnect from the currently connected Wi-Fi access point.
+    WifiDisconnect,
+    /// Connect to the TCP server with the given IP address and port number.
+    TcpConnect(u32, u16),
+    // TcpWrite(&'a [u8]),
+    // TcpRead,
+    /// Close the currently active TCP connection.
+    TcpClose,
 }
 
 impl<'a> Encode<'a> for Request<'a> {}
@@ -28,15 +41,39 @@ impl<'a> Encode<'a> for Request<'a> {}
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub enum Response<'a> {
     Error(&'a str),
+
+    /// Confirmation for [`Request::NetStart`].
     NetStarted,
+    /// Confirmation for [`Request::NetStop`].
     NetStopped,
+    /// Respnse for [`Request::NetLocalAddr`].
     NetLocalAddr([u8; 6]),
+    /// Confirmation for [`Request::NetAdvertise`].
     NetAdvertised,
     NetIncoming([u8; 6], &'a [u8]),
     NetNoIncoming,
+    /// Confirmation for [`Request::NetSend`].
     NetSent,
+    /// Respnse for [`Request::NetSendStatus`].
     NetSendStatus(SendStatus),
+
     Input(Option<(u16, u16)>, u8),
+
+    /// List of SSIDs of top 6 available Wi-Fi Access Points.
+    ///
+    /// SSID is up to 30 bytes. 6 SSIDs take up to 180 bits.
+    /// The SPI packet size is limited to 255 bits
+    /// because we use a single byte to transfer the packet size.
+    WifiScan([&'a str; 6]),
+    WifiConnected,
+    WifiDisconnected,
+
+    TcpConnected,
+    TcpWritten,
+    TcpClosed,
+    TcpRespStart(u16),
+    TcpRespChunk(&'a [u8]),
+    TcpRespEnd,
 }
 
 impl<'a> Encode<'a> for Response<'a> {}
