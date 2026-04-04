2f72efb5751081f41fd9f6050820d27d73263b4d
gram <git@orsinium.dev>
2026-04-04 08:20:05 +0200

    docstrings for spi messages

--- src/spi.rs
+++ src/spi.rs
@@ -21,20 +21,39 @@ pub enum Request<'a> {
     /// Get the latest touchpad and buttons inputs.
     ReadInput,
 
-    /// Scan the air for available non-hidden Wi-Fi access points.
+    /// Scan the air for available non-hidden wifi access points.
     WifiScan,
+
     /// Connect to an access point using the given SSID and password.
+    ///
+    /// Async. Check [`Request::WifiStatus`] to see if the device is actually connected.
     WifiConnect(&'a str, &'a str),
+
+    /// Get the current connection status to an AP.
+    ///
+    /// Since [`Request::WifiConnect`] is async, make sure to account for races.
+    /// For instance, shortly after requesting a connect, the status might
+    /// still indicate the status of the previous connection.
     WifiStatus,
-    /// Disconnect from the currently connected Wi-Fi access point.
+
+    /// Disconnect from the currently connected wifi access point.
     WifiDisconnect,
+
     /// Connect to the TCP server with the given IP address and port number.
+    ///
+    /// There can be only one open TCP connection at a time.
     TcpConnect(u32, u16),
+
     /// Fetch the state of the currently open TCP connection.
     TcpStatus,
+
+    /// Send the given bytes into the currently open TCP connection.
     TcpSend(&'a [u8]),
+
+    /// Read a bytes chunk from the currently open TCP connection.
     TcpRecv,
-    /// Close the currently active TCP connection.
+
+    /// Close the currently open TCP connection.
     TcpClose,
 }
 
@@ -49,7 +68,7 @@ pub enum Response<'a> {
     NetStarted,
     /// Confirmation for [`Request::NetStop`].
     NetStopped,
-    /// Respnse for [`Request::NetLocalAddr`].
+    /// Response for [`Request::NetLocalAddr`].
     NetLocalAddr([u8; 6]),
     /// Confirmation for [`Request::NetAdvertise`].
     NetAdvertised,
@@ -57,25 +76,53 @@ pub enum Response<'a> {
     NetNoIncoming,
     /// Confirmation for [`Request::NetSend`].
     NetSent,
-    /// Respnse for [`Request::NetSendStatus`].
+    /// Response for [`Request::NetSendStatus`].
     NetSendStatus(SendStatus),
 
+    /// Response for [`Request::ReadInput`].
+    ///
+    /// The first item is the touch coordinates on the pad (if any).
+    /// The second is serialized bitflags of pressed buttons.
     Input(Option<(u16, u16)>, u8),
 
-    /// List of SSIDs of top 6 available Wi-Fi Access Points.
+    /// List of SSIDs of up to 6 available wifi Access Points.
+    ///
+    /// Includes 6 of the first detected APs.
+    /// So, it's not the top closest APs but the closer AP
+    /// the higher its chance to make it to the list.
     ///
     /// SSID is up to 30 bytes. 6 SSIDs take up to 180 bits.
     /// The SPI packet size is limited to 255 bits
     /// because we use a single byte to transfer the packet size.
     WifiScan([&'a str; 6]),
-    WifiStatus(u8),
-    WifiConnected,
-    WifiDisconnected,
 
+    /// The status of current connection to a wifi AP.
+    ///
+    /// For firefly-types and firefly-runtime, it's opaque.
+    /// It doesn't know which integer corresponds to which state.
+    /// The status is encoded in firefly-io and decoded in firefly-installer.
+    /// firefly-hal is also aware of status encoding, though, for the purpose
+    /// of mocking wifi on the hosted environment (emulator).
+    WifiStatus(u8),
+
+    /// Confirmation for [`Request::WifiConnect`].
+    ///
+    /// The connection request is async, so the response doesn't mean
+    /// that the device is actually connected to the AP (yet).
+    /// Use [`Request::WifiStatus`] to get the actual connection status.
+    WifiConnected,
+
+    /// Confirmation for [`Request::WifiDisconnect`].
+    WifiDisconnected,
+    /// Confirmation for [`Request::TcpConnect`].
     TcpConnected,
+    /// Response for [`Request::TcpStatus`].
     TcpStatus(u8),
+    /// Confirmation for [`Request::TcpSend`].
     TcpSent,
+    /// Response for [`Request::TcpRecv`].
     TcpChunk(&'a [u8]),
+    /// Confirmation for [`Request::TcpClose`].
     TcpClosed,
 }
 
